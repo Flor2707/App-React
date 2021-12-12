9b79cc3ace9888ddfcaa9b876c5d23987fbd9a3c
Flor <florencialopez2707@gmail.com>
2021-12-11 21:11:51 -0300

    style

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,16 +1,36 @@
 import React from 'react'
 import Cardwidget from '../CartWidget'
 
+const styles = {
+    link: {
+        textDecoration: "none",
+        color: "white",
+        margin: "10px",
+    },
+    list: {
+        display:"flex",
+        justifyContent: "flex-end",
+        listStyle: "none",
+
+    },
+    nav: {
+        backgroundColor: "pink",
+    },
+};
+
+
 const NavBar = ({titulo}) => {
     return (
         <div>
             <h1>{titulo}</h1>
-            <ul>
-                <li><a href='#'>Productos</a></li>
-                <li><a href='#'>Nosotros</a></li>
-                <li><a href='#'>Contacto</a></li>
+            <nav style={styles.nav}>
+            <ul  style={styles.list}>
+                <li><a href='#' style={styles.link} >Productos</a></li>
+                <li><a href='#'  style={styles.link}>Nosotros</a></li>
+                <li><a href='#'  style={styles.link}>Contacto</a></li>
             </ul>
             <Cardwidget/>
+            </nav>
         </div>
     )
 }
